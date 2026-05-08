7c05deda2c4dd7bcc172f45b502832d2f4fb07ec
AV-Alumno <AV-Alumno@duoc.acad>
2026-05-08 14:07:56 -0400

    se agregó lo faltante en properties y se creo la tabla de medico

---
+++ src/main/resources/db/changelog/db.changelog.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+
+
+--changeset pedro:1
+CREATE TABLE medico (
+    id_medico BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL,
+    prevision VARCHAR(255) NOT NULL
+);
