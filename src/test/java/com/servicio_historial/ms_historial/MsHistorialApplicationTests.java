cb5be0fe2ed89f2766656270cd0a2b1c134e4744
PabloDev <pe.philimon@duocuc.cl>
2026-05-16 15:05:36 -0400

    se añadio el nuevo microservicio historial medico

---
+++ src/test/java/com/servicio_historial/ms_historial/MsHistorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio_historial.ms_historial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
