4d0ceed7fce74e474d8ffc682cd1c2c5aaee3a00
PabloDev <pe.philimon@duocuc.cl>
2026-05-18 23:59:24 -0400

    se modificó db.changelog.sql

--- ms-historial/src/main/resources/db/changelog/db.changelog.sql
+++ ms-historial/src/main/resources/db/changelog/db.changelog.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+
+-- changeset pedro:1
 CREATE TABLE historiales_clinicos (
     id_historial BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_paciente BIGINT NOT NULL,
@@ -7,13 +10,14 @@ CREATE TABLE historiales_clinicos (
     tratamiento VARCHAR(255) NOT NULL
 );
 
+-- changeset pedro:2
 CREATE TABLE registros_clinicos (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_historial BIGINT NOT NULL,
-    peso VARCHAR(50),
-    presion_arterial VARCHAR(50),
-    temperatura VARCHAR(50),
-    observaciones VARCHAR(255),
+    peso VARCHAR(50) NOT NULL,
+    presion_arterial VARCHAR(50) NOT NULL,
+    temperatura VARCHAR(50) NOT NULL,
+    observaciones VARCHAR(255) NOT NULL,
     CONSTRAINT fk_historial_registro FOREIGN KEY (id_historial) 
         REFERENCES historiales_clinicos(id_historial) ON DELETE CASCADE
-);  
+);
