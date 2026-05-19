769bdd8330442e0871013b18c95890e170f67069
PabloDev <pe.philimon@duocuc.cl>
2026-05-19 11:51:08 -0400

    se modificó pacienteCLient

--- ms-laboratorio/src/main/java/com/servicio_laboratorio/ms_laboratorio/dto/OrdenRequestDTO.java
+++ ms-laboratorio/src/main/java/com/servicio_laboratorio/ms_laboratorio/dto/OrdenRequestDTO.java
@@ -3,11 +3,15 @@ package com.servicio_laboratorio.ms_laboratorio.dto;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.FutureOrPresent;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrdenRequestDTO {
     
     private Long id;
