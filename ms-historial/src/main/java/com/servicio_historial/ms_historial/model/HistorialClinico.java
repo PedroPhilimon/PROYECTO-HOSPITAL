a48c7037a0950dd5578e21732f1dcad59505ed4f
PabloDev <pe.philimon@duocuc.cl>
2026-05-18 23:10:15 -0400

    Se modifico HistorialClinico

--- ms-historial/src/main/java/com/servicio_historial/ms_historial/model/HistorialClinico.java
+++ ms-historial/src/main/java/com/servicio_historial/ms_historial/model/HistorialClinico.java
@@ -1,6 +1,8 @@
 package com.servicio_historial.ms_historial.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +18,15 @@ public class HistorialClinico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idHistorial;
     
+    @NotNull(message = "El id del paciente no puede ser nulo")
     private Long idPaciente;
+    @NotNull(message = "El id del médico no puede ser nulo")
     private Long idMedico;
     private LocalDateTime fechaAtencion;
+
+    @NotBlank(message = "El diagnóstico no puede estar vacío")
     private String diagnostico;
+
+    @NotBlank(message = "El diagnóstico no puede estar vacío")
     private String tratamiento;
 }
