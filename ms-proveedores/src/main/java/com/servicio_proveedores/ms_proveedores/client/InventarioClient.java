725219f1aa3d9435bf02935094a60c9708d4eeed
PabloDev <pe.philimon@duocuc.cl>
2026-05-19 11:17:13 -0400

    se modifica el client de inventario

--- ms-proveedores/src/main/java/com/servicio_proveedores/ms_proveedores/client/InventarioClient.java
+++ ms-proveedores/src/main/java/com/servicio_proveedores/ms_proveedores/client/InventarioClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "ms-inventario", url = "http://localhost:8083/api/inventario") 
+@FeignClient(name = "ms-inventario", url = "http://localhost:8083/api/productos") 
 public interface InventarioClient {
 
     @GetMapping("/{id}")
