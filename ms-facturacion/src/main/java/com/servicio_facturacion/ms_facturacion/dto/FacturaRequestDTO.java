4eea9efea9dca85585ce0a9398dedf55c6a9c966
PabloDev <pe.philimon@duocuc.cl>
2026-05-18 22:48:24 -0400

    se modifico FacturaRequestDTO

--- ms-facturacion/src/main/java/com/servicio_facturacion/ms_facturacion/dto/FacturaRequestDTO.java
+++ ms-facturacion/src/main/java/com/servicio_facturacion/ms_facturacion/dto/FacturaRequestDTO.java
@@ -6,11 +6,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FacturaRequestDTO {
     
     private Long id;
