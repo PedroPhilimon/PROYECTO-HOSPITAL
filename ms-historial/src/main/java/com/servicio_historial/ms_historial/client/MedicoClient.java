7f8c72a4d7f52390fbdb73f37b845c3919d96f39
PedroPhilimon <pe.philimon@duocuc.cl>
2026-05-20 22:43:13 -0400

    se modifca MedicoClient y PacienteClient

--- ms-historial/src/main/java/com/servicio_historial/ms_historial/client/MedicoClient.java
+++ ms-historial/src/main/java/com/servicio_historial/ms_historial/client/MedicoClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-medicos", url = "http://localhost:8081")
+@FeignClient(name = "ms-medicos", url = "http://localhost:8081/api/medicos")
 public interface MedicoClient {
 
-    @GetMapping("/api/medicos/{id}")
+    @GetMapping("/{id}")
     Object buscarPorId(@PathVariable("id") Long id);
 }
