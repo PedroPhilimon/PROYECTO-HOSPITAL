28d41bb548d46715424af76d4aed060ee6c5b787
PedroPhilimon <pe.philimon@duocuc.cl>
2026-05-20 22:46:04 -0400

    Se modifica CitaClient y app.properties

--- ms-inventario/src/main/java/com/servicio_inventario/ms_inventario/client/CitaClient.java
+++ ms-inventario/src/main/java/com/servicio_inventario/ms_inventario/client/CitaClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-citamedica", url = "http://localhost:8082")
+@FeignClient(name = "ms-citamedica", url = "http://localhost:8082/api/citas")
 public interface CitaClient {
 
-    @GetMapping("api/citas/{id}")
+    @GetMapping("/{id}")
     Object buscarPorId(@PathVariable("id") Long id);
 }
