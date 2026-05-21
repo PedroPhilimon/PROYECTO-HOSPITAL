e9477bd13d4944287bc92bc5f44c0384ac03ad7c
PedroPhilimon <pe.philimon@duocuc.cl>
2026-05-21 00:07:11 -0400

    se modifico PacienteClient

--- ms-citamedica/src/main/java/com/servicio_citamedica/ms_citamedica/client/PacienteClient.java
+++ ms-citamedica/src/main/java/com/servicio_citamedica/ms_citamedica/client/PacienteClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-pacientes", url = "http://localhost:8090/api/pacientes")
 public interface PacienteClient {
 
-    @GetMapping("/api/pacientes/{id}")
+    @GetMapping("/{id}")
     Object buscarPorId(@PathVariable("id") Long id); 
 }
