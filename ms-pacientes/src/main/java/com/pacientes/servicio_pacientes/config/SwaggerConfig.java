6b4cead67cf309290e7b1082722455e1ed341776
Ewann777 <evancesar.markez@gmail.com>
2026-06-11 22:11:56 -0400

    Añadí documentacion al controlador paciente

---
+++ ms-pacientes/src/main/java/com/pacientes/servicio_pacientes/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.pacientes.servicio_pacientes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.aspectj.apache.bcel.generic.RET;
+import org.springdoc.webmvc.api.OpenApiActuatorResource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info().title("API Gestión Pacientes").version("1.0").description("Documentación de la API para el microservicio de pacientes")
+                );
+    }
+}
--- ms-pacientes/src/main/java/com/pacientes/servicio_pacientes/controller/PacienteController.java
+++ ms-pacientes/src/main/java/com/pacientes/servicio_pacientes/controller/PacienteController.java
@@ -2,7 +2,15 @@ package com.pacientes.servicio_pacientes.controller;
 
 import java.util.List;
 
+import com.pacientes.servicio_pacientes.model.Paciente;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +27,12 @@ import com.pacientes.servicio_pacientes.service.PacienteService;
 
 import jakarta.validation.Valid;
 
+import javax.print.attribute.standard.Media;
+
+
 @RestController
 @RequestMapping("/api/pacientes")
+@Tag(name = "Pacientes", description = "Operaciones relacionadas con los pacientes")
 public class PacienteController {
 
     private final PacienteService pacienteService;
@@ -32,6 +44,7 @@ public class PacienteController {
 
 
     @GetMapping
+    @Operation(summary = "Obtiene todos los pacientes", description = "Obtiene una lista de todos los pacientes")
     public ResponseEntity<List<PacienteResponseDTO>> findAll() {
         List<PacienteResponseDTO> pacientes = pacienteService.findAll();
         return ResponseEntity.ok(pacientes);
@@ -50,6 +63,15 @@ public class PacienteController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualiza un paciente", description = "Actualiza el paciente y lo guarda en la db")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Paciente actualizado exitosamente!",
+                        content = @Content(mediaType = "Aplication/json",
+                            schema = @Schema(implementation = Paciente.class))),
+                    @ApiResponse(responseCode = "404", description = "Paciente no encontrado D:")
+            }
+    )
     public ResponseEntity<PacienteResponseDTO> update(@PathVariable Long id, @Valid @RequestBody PacienteRequestDTO dto) {
         PacienteResponseDTO actualizarPaciente = pacienteService.update(id, dto);
         return ResponseEntity.ok(actualizarPaciente);
