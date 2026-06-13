d63b965e2de3bdf5ce87912af1264b86f477a135
PedroPhilimon <pe.philimon@duocuc.cl>
2026-06-13 17:58:50 -0400

    Se agrega la dependencia de liquibase, y se insertan datos en sql y se arreglan los errrores de nombres

--- ms-citamedica/src/main/resources/db/changelog/db.changelog.sql
+++ ms-citamedica/src/main/resources/db/changelog/db.changelog.sql
@@ -4,23 +4,30 @@ DROP TABLE IF EXISTS citas;
 DROP TABLE IF EXISTS salas;
 DROP TABLE IF EXISTS databasechangelog;
 DROP TABLE IF EXISTS databasechangeloglock;
+
 --changeset pedro:1
 CREATE TABLE salas (
     id_sala BIGINT AUTO_INCREMENT PRIMARY KEY,
-    NOMBRE VARCHAR(100) NOT NULL,
-    UBICACION VARCHAR(255)
+    nombre VARCHAR(100) NOT NULL,
+    ubicacion VARCHAR(255)
 );
 
 --changeset pedro:2
 CREATE TABLE citas (
     id_cita BIGINT AUTO_INCREMENT PRIMARY KEY,
-    paciente_id BIGINT NOT NULL,
-    medico_id BIGINT NOT NULL,
+    pacienteId BIGINT NOT NULL,
+    medicoId BIGINT NOT NULL,
     sala_id BIGINT, -- Columna añadida para la relación
     fecha DATE NOT NULL,
     hora TIME NOT NULL,
     estado VARCHAR(50) NOT NULL,
     motivo VARCHAR(255),
-    observacion VARCHAR(500),
-    CONSTRAINT FK_SALA_CITA FOREIGN KEY (sala_id) REFERENCES SALA_ATENCION(id_sala)
+    CONSTRAINT FK_SALA_CITA FOREIGN KEY (sala_id) REFERENCES salas(id_sala)
 ); 
+
+--changeset pedro:3
+INSERT INTO salas (nombre, ubicacion) VALUES ('Sala de Urgencias A', 'Piso 1 - Ala Norte');
+INSERT INTO salas (nombre, ubicacion) VALUES ('Pabellón de Cirugía General', 'Piso 2 - Bloque B');
+INSERT INTO salas (nombre, ubicacion) VALUES ('Consulta Pediatría', 'Piso 1 - Ala Sur');
+INSERT INTO salas (nombre, ubicacion) VALUES ('Box de Cardiología', 'Piso 3 - Consultas Médicas');
+INSERT INTO salas (nombre, ubicacion) VALUES ('Unidad de Cuidados Intensivos (UCI)', 'Piso 2 - Bloque C');
