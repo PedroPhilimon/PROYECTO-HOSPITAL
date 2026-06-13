58f2cd6e5d1cab8bb327262fc30025303d84da55
PedroPhilimon <pe.philimon@duocuc.cl>
2026-06-13 17:38:15 -0400

    Se modifica los errores de SalaAtencionController y se coloca None en application.properties

--- ms-citamedica/src/main/java/com/servicio_citamedica/ms_citamedica/controller/SalaAtencionController.java
+++ ms-citamedica/src/main/java/com/servicio_citamedica/ms_citamedica/controller/SalaAtencionController.java
@@ -2,7 +2,11 @@ package com.servicio_citamedica.ms_citamedica.controller;
 
 import com.servicio_citamedica.ms_citamedica.dto.CitaRequestDTO;
 import com.servicio_citamedica.ms_citamedica.dto.CitaResponseDTO;
+import com.servicio_citamedica.ms_citamedica.dto.SalaAtencionRequestDTO;
+import com.servicio_citamedica.ms_citamedica.dto.SalaAtencionResponseDTO;
 import com.servicio_citamedica.ms_citamedica.service.CitaMedicaService;
+import com.servicio_citamedica.ms_citamedica.service.SalaAtencionService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,45 +18,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/citas")
-@Tag(name = "Citas Médicas", description = "Controller para la programación y gestión de citas de pacientes")
-public class CitaMedicaController {
+@RequestMapping("/api/salas")
+@Tag(name = "Gestión Salas del hospital", description = "Gestiona todas las salas de un hospital")
+public class SalaAtencionController {
 
     @Autowired
-    private CitaMedicaService citaMedicaService;
+    private SalaAtencionService salaAtencionService;
 
     @PostMapping
-    @Operation(summary = "Agendar una cita médica", description = "Crea una nueva cita vinculando a un paciente, un médico y un horario")
-    @ApiResponse(responseCode = "201", description = "Cita agendada exitosamente")
-    @ApiResponse(responseCode = "400", description = "Error en la validación de datos o conflicto de horario")
-    public ResponseEntity<CitaResponseDTO> crearCita(@RequestBody CitaRequestDTO requestDTO) {
-        CitaResponseDTO responseDTO = citaMedicaService.crearCita(requestDTO);
+    @Operation(summary = "Crear una sala", description = "Crea una sala con todos sus detalles")
+    @ApiResponse(responseCode = "201", description = "Sala Creada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Error al crear la Sala")
+    public ResponseEntity<SalaAtencionResponseDTO> crearSala(@RequestBody SalaAtencionRequestDTO requestDTO) {
+        SalaAtencionResponseDTO responseDTO = salaAtencionService.create(requestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Obtener detalles de una cita", description = "Busca y retorna la información de una cita específica por su ID")
-    @ApiResponse(responseCode = "200", description = "Cita encontrada")
-    @ApiResponse(responseCode = "404", description = "Cita no encontrada")
-    public ResponseEntity<CitaResponseDTO> obtenerCitaPorId(@PathVariable Long id) {
-        CitaResponseDTO responseDTO = citaMedicaService.obtenerCitaPorId(id);
+    @Operation(summary = "Obtener detalles de una sala específica", description = "Busca y retorna la información de una sala específica por su ID")
+    @ApiResponse(responseCode = "200", description = "Sala encontrada")
+    @ApiResponse(responseCode = "404", description = "Sala no encontrada")
+    public ResponseEntity<SalaAtencionResponseDTO> obtenerSalaId(@PathVariable Long id) {
+        SalaAtencionResponseDTO responseDTO = salaAtencionService.findById(id);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @GetMapping
-    @Operation(summary = "Listar todas las citas", description = "Retorna el historial de todas las citas médicas registradas")
+    @Operation(summary = "Listar todas las salas", description = "Retorna todas las salas registradas")
     @ApiResponse(responseCode = "200", description = "Lista obtenida exitosamente")
-    public ResponseEntity<List<CitaResponseDTO>> listarCitas() {
-        List<CitaResponseDTO> citas = citaMedicaService.listarCitas();
+    public ResponseEntity<List<SalaAtencionResponseDTO>> listarSalas() {
+        List<SalaAtencionResponseDTO> citas = salaAtencionService.findAll();
         return new ResponseEntity<>(citas, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Modificar una cita médica", description = "Permite reprogramar o actualizar los detalles de una cita existente")
-    @ApiResponse(responseCode = "200", description = "Cita modificada exitosamente")
-    @ApiResponse(responseCode = "404", description = "Cita no encontrada")
-    public ResponseEntity<CitaResponseDTO> actualizarCita(@PathVariable Long id, @RequestBody CitaRequestDTO requestDTO) {
-        CitaResponseDTO responseDTO = citaMedicaService.actualizarCita(id, requestDTO);
+    @Operation(summary = "Modificar una sala específica", description = "Modificar una sala existente según su id")
+    @ApiResponse(responseCode = "200", description = "Sala modificada exitosamente")
+    @ApiResponse(responseCode = "404", description = "Sala no encontrada")
+    public ResponseEntity<SalaAtencionResponseDTO> actualizarSala(@PathVariable Long id, @RequestBody SalaAtencionRequestDTO requestDTO) {
+        SalaAtencionResponseDTO responseDTO = salaAtencionService.update(id, requestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
@@ -60,8 +64,8 @@ public class CitaMedicaController {
     @Operation(summary = "Cancelar una cita médica", description = "Elimina o cancela el registro de la cita seleccionada")
     @ApiResponse(responseCode = "204", description = "Cita cancelada exitosamente")
     @ApiResponse(responseCode = "404", description = "Cita no encontrada")
-    public ResponseEntity<Void> eliminarCita(@PathVariable Long id) {
-        citaMedicaService.eliminarCita(id);
+    public ResponseEntity<Void> eliminarSala(@PathVariable Long id) {
+        salaAtencionService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
