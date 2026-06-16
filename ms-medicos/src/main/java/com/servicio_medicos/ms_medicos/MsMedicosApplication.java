18806247ca00424fea13ce5f3aa1e5df81b94608
PedroPhilimon <pe.philimon@duocuc.cl>
2026-06-15 23:03:07 -0400

    se arreglan errores de nombres en app.yml, Se habilita eureka discovery client en ms-medicos

--- ms-medicos/src/main/java/com/servicio_medicos/ms_medicos/MsMedicosApplication.java
+++ ms-medicos/src/main/java/com/servicio_medicos/ms_medicos/MsMedicosApplication.java
@@ -2,8 +2,10 @@ package com.servicio_medicos.ms_medicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsMedicosApplication {
 
 	public static void main(String[] args) {
