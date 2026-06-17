9d5c818492badaa2ab73459de89405f3dc9c4c22
PedroPhilimon <pe.philimon@duocuc.cl>
2026-06-17 18:21:12 -0400

    se habilita arregla el error de ms-auth con eureka, se modifica el pom de ms-laboratorio y su error al no registrarse en eureka

--- ms-auth/src/main/java/com/microservicio_auth/ms_auth/MsAuthApplication.java
+++ ms-auth/src/main/java/com/microservicio_auth/ms_auth/MsAuthApplication.java
@@ -2,6 +2,7 @@ package com.microservicio_auth.ms_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // <-- Agrega esta línea
 
 @SpringBootApplication
 @EnableDiscoveryClient
