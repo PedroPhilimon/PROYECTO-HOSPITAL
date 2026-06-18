c8046a2b062a787e38e33e2dbb7456aed1feb4be
PedroPhilimon <pe.philimon@duocuc.cl>
2026-06-18 11:58:10 -0400

    se arreglan errores de conexiones por feign

--- ms-laboratorio/src/main/java/com/servicio_laboratorio/ms_laboratorio/client/ConsultaClient.java
+++ ms-laboratorio/src/main/java/com/servicio_laboratorio/ms_laboratorio/client/ConsultaClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-consultas", url = "http://localhost:8087/api/consultas")
+@FeignClient(name = "ms-consultas", url = "http://hospital-ms-consultas:8087/api/consultas")
 public interface ConsultaClient {
 
     @GetMapping("/{id}")
